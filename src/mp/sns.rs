1a37ed409cfee92bf61e32af596d14f824f686b4
rise0chen <rise0chen@163.com>
2025-06-22 18:19:47 +0800

    perf

--- src/mp/sns.rs
+++ src/mp/sns.rs
@@ -3,25 +3,25 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct GrantAccessToken {
-    access_token: String,
-    expires_in: u32,
-    refresh_token: String,
-    openid: String,
-    scope: String,
-    unionid: Option<String>,
+    pub access_token: String,
+    pub expires_in: u32,
+    pub refresh_token: String,
+    pub openid: String,
+    pub scope: String,
+    pub unionid: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct UserInfo {
-    openid: String,
-    nickname: String,
-    sex: u8,
-    province: String,
-    city: String,
-    country: String,
-    headimgurl: String,
-    privilege: Option<Vec<String>>,
-    unionid: Option<String>,
+    pub openid: String,
+    pub nickname: String,
+    pub sex: u8,
+    pub province: String,
+    pub city: String,
+    pub country: String,
+    pub headimgurl: String,
+    pub privilege: Option<Vec<String>>,
+    pub unionid: Option<String>,
 }
 
 pub struct SnsModule<'a, T: AccessTokenProvider>(pub(crate) &'a WxSdk<T>);
@@ -30,17 +30,17 @@ impl<'a, T: AccessTokenProvider> SnsModule<'a, T> {
     /// 微信网页授权
     /// 详情请参考[微信官方文档](https://developers.weixin.qq.com/doc/offiaccount/OA_Web_Apps/Wechat_webpage_authorization.html)
     /// 第二步，通过code换取access token
-    pub async fn oauth_access_token(&self, code: String) -> SdkResult<GrantAccessToken> {
+    pub async fn oauth_access_token(&self, code: &str) -> SdkResult<GrantAccessToken> {
         let base_url = "https://api.weixin.qq.com/sns/oauth2/access_token";
         let client = self.0.http_client.get(base_url);
 
         let app_id = self.0.app_id.clone();
         let app_secret = self.0.app_secret.clone();
         let client = client.query(&[
-            ("appid", app_id),
-            ("secret", app_secret),
+            ("appid", app_id.as_str()),
+            ("secret", app_secret.as_str()),
             ("code", code),
-            ("grant_type", "authorization_code".to_owned()),
+            ("grant_type", "authorization_code"),
         ]);
         let res: CommonResponse<GrantAccessToken> = client.send().await?.json().await?;
         res.into()
